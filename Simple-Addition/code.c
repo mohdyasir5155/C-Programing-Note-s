a254eb22be8d77abc96db6bac7a4440c4ff2c87b
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-03 20:27:55 +0530

    Add files via upload

---
+++ Simple-Addition/code.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1 = 99;
+    int num2 = 33;
+    int sum;
+
+    sum = num1 + num2;
+    printf("The sum of addition of two number's are = (%d)", sum);   
+}

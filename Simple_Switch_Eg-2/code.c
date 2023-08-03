cd74515cb99670ae1d2ffca142eab5dc48db8be2
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-03 21:06:44 +0530

    Add files via upload

---
+++ Simple_Switch_Eg-2/code.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int day = 4;
+
+    switch (day)
+    {
+        case 1:
+        printf("Today is Saturday!🪐");
+        break;
+
+        case 2:
+        printf("Today is Sunday!🎉");
+        break;
+
+        default:
+        printf("Looking forward to the Weekend!🥱");
+    }
+    return 0;
+
+}

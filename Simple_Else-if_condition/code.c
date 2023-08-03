d8887ebbfcaff30e37aac69e2845d9c2b0ab4d36
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-03 20:55:35 +0530

    Add files via upload

---
+++ Simple_Else-if_condition/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x = 22;
+    if(x < 10)
+    {
+        printf("Hi!✋");
+    }else if (x < 20)
+    {
+        printf("Hello!👋");
+    }else
+    {
+        printf("Bye🙋‍♂️");
+    }
+}

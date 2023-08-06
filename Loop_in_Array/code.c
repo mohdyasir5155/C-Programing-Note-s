70bbcb542750e046ef5f167ffa4a77ef21be0088
Mohd Yasir Sheikh <mohdyasir5155@gmail.com>
2023-08-06 12:38:36 +0530

    Add files via upload

---
+++ Loop_in_Array/code.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int myNumber[] = {25, 50 , 75, 100};
+    int i;
+    for(i=0; i< 4; i++){
+        printf("%d\n", myNumber[i]);
+    }
+}
---
+++ Simple_Array/code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int myNumbers[] = {25, 50, 75, 100};
+    myNumbers [0] = 33;
+
+    printf("%d", myNumbers[0]);
+}
+
+//Now output is 33 instead of 25
+
+//By Mohd Yasir Sheikh😉
